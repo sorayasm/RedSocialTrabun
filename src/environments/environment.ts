942e914c5327e870d2312fa9b6b11e7b2a4bbb08
Tamara Hinojosa Salinas <tamara.hi.sa@hotmail.com>
2018-09-20 22:01:28 -0300

    Se arreglan issues con comillas dobles

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyChiZJ0h46EgQ80KbVvPChM4RwiijFlKl0",
-    authDomain: "trabun-4b6cd.firebaseapp.com",
-    databaseURL: "https://trabun-4b6cd.firebaseio.com",
-    projectId: "trabun-4b6cd",
-    storageBucket: "trabun-4b6cd.appspot.com",
-    messagingSenderId: "410021138457"
+    apiKey: 'AIzaSyChiZJ0h46EgQ80KbVvPChM4RwiijFlKl0',
+    authDomain: 'trabun-4b6cd.firebaseapp.com',
+    databaseURL: 'https://trabun-4b6cd.firebaseio.com',
+    projectId: 'trabun-4b6cd',
+    storageBucket: 'trabun-4b6cd.appspot.com',
+    messagingSenderId: '410021138457'
   }
 };
 
