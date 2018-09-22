0768161c7f85a6146a34b8cb6fcc76978759df4e
sorayasm <soraya.sacaan@gmail.com>
2018-09-22 10:43:53 -0300

    navbar terminada

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -2,14 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { Observable } from 'rxjs';
 
+
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
+  users$:Observable<any>; 
+  constructor(private database:AngularFireDatabase) { 
 
-  constructor(private database:AngularFireDatabase) { }
+    this.users$ = this.database.list('/users').valueChanges();
+  }
 
   ngOnInit() {
   }
