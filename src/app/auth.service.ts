ce9ccf7546bd9090b1c3ffa12ce3608b64e21e19
Tammy <tamara.hi.sa@hotmail.com>
2018-09-26 15:02:00 -0300

    comit pull errores, app queda ok con login social funcional

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -12,17 +12,19 @@ import { AngularFireList } from '@angular/fire/database';
 })
 
 export class AuthService {
-  public user: Observable<firebase.User>;
-  public userDetails: firebase.User = null;
-  userList$: AngularFireList<any>;
+  public user$: Observable<firebase.User>;
+  public userDetails$: firebase.User = null;
+  public userList$: AngularFireList<any>;
+
   constructor(public firebaseAuth: AngularFireAuth, public database: AngularFireDatabase, public router: Router ) {
+    this.user$ = firebaseAuth.authState;
     this.userList$ = this.database.list('/users');
-    this.user.subscribe(
+    this.user$.subscribe(
     (user) => {
       if (user) {
-        this.userDetails = user;
+        this.userDetails$ = user;
       } else {
-        this.userDetails = null;
+        this.userDetails$ = null;
       }
     }
   );
@@ -66,7 +68,7 @@ export class AuthService {
   }
 
   isLoggedIn() {
-    if (this.userDetails == null ) {
+    if (this.userDetails$ == null ) {
         return false;
       } else {
         return true;
